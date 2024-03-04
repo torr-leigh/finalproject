ea16040afdf602117f4f722b4cb386b74b834566
Leigh Torrence <156026857+torr-leigh@users.noreply.github.com>
2024-03-04 11:21:04 -0800

    basemap established

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+var map = L.map('map').setView([39.8283, -98.5795], 4);
+
+
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}).addTo(map);
